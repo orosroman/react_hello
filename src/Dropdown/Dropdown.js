60ea8bf78ebd352f23e8db73e52aa174a56e9690
Roman Oros <roros@softserveinc.com>
2017-04-20 20:14:08 +0300

    lesson 4

---
+++ src/Dropdown/Dropdown.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './Dropdown.css';
+
+class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isOpened: false};
+    }
+
+    toggleState() {
+        this.setState({isOpened: !this.state.isOpened});
+    }
+
+    render() {
+        console.log('isOpened', this.state.isOpened);
+        let dropdownText;
+        if (this.state.isOpened) {
+            dropdownText = <div>Here is what is shown in dropdown</div>
+        }
+        return (
+            <div onClick={this.toggleState.bind(this)}>
+                It is dropdown
+                {dropdownText}
+            </div>
+        );
+    }
+}
+
+export default Dropdown;
---
+++ src/Header/Header.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import {PropTypes} from 'prop-types';
+
+class Header extends Component {
+    static propTypes = {
+        items: PropTypes.array.isRequired,
+        isLoading: PropTypes.bool,
+        submit: PropTypes.func,
+        title: PropTypes.string,
+        type: PropTypes.oneOf(['news', 'photos']),
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            age: PropTypes.number
+        }),
+        users: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string,
+                age: PropTypes.number
+            })
+        )
+    }
+
+    render() {
+        console.log(this.props.items);
+        return (
+            <div>
+                {this.props.items.map((item, index) => 
+                    <a href={item.link} key={index}>{item.label}</a>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Header;
